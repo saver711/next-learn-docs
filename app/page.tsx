58df5706bf9772c28e377dea8a66cea639ba8205
ahmed hassan <ahmed.hassan@brightskiesinc.com>
2025-01-11 15:44:01 +0200

    change description

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,8 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal font-lusitana`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> Example for the Next.js best
+            practices by vercel + My Enhancements
           </p>
           <Link
             href="/login"
